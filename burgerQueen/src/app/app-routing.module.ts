1b473a5188beb3bfa99e7e6985b9f28fcba5786b
Karen <krufino@itech-sac.com>
2022-02-14 11:10:06 -0500

    creando componentes

--- burgerQueen/src/app/app-routing.module.ts
+++ burgerQueen/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PgeNotFoundComponent } from './pge-not-found/pge-not-found.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {path:'app-login', component:LoginComponent},
+    {path: 'app-welcome', component:WelcomeComponent},
+    {path: 'app-sign-up', component:SignUpComponent},
+    {path: '', redirectTo: '/app-welcome', pathMatch: 'full'},
+    {path: '**', component: PgeNotFoundComponent}
+  ])
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- burgerQueen/src/app/app.module.ts
+++ burgerQueen/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PedidosComponent } from './pedidos/pedidos.component';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PgeNotFoundComponent } from './pge-not-found/pge-not-found.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PedidosComponent,
-    LoginComponent
+    LoginComponent,
+    WelcomeComponent,
+    PgeNotFoundComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
