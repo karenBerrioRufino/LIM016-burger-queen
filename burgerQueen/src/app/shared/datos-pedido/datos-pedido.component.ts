ee71edc8c0b6dbdf7b6088730faa218020e65b6e
Karen <krufino@itech-sac.com>
2022-02-16 17:52:14 -0500

    modulos

---
+++ burgerQueen/src/app/shared/datos-pedido/datos-pedido.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datos-pedido',
+  templateUrl: './datos-pedido.component.html',
+  styleUrls: ['./datos-pedido.component.scss']
+})
+export class DatosPedidoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ burgerQueen/src/app/shared/total-pedidos/total-pedidos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-total-pedidos',
+  templateUrl: './total-pedidos.component.html',
+  styleUrls: ['./total-pedidos.component.scss']
+})
+export class TotalPedidosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
