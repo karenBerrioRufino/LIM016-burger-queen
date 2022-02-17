f79915871d95eb2861660a0834ef0cffc67f872e
andreahenos <henostroza.andrea@gmail.com>
2022-02-17 10:41:12 -0500

    Ruteo de la vista de ingreso (Bienvenida y Login)

--- burgerQueen/src/app/ingreso/ingreso-routing.module.ts
+++ burgerQueen/src/app/ingreso/ingreso-routing.module.ts
@@ -8,9 +8,9 @@ const routes: Routes = [
   {
     path: '',
     children: [
-      { path: 'app-welcome', component: WelcomeComponent },
-      { path: 'app-login', component: LoginComponent },
-      {path: '**', redirectTo: 'app-welcome', pathMatch: 'full'},
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'login', component: LoginComponent },
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'},
     ]
   }
 ]
