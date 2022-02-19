c97e157ba46341bc4809f93ec89e7a2369e383d6
Karen <krufino@itech-sac.com>
2022-02-18 20:13:46 -0500

    firebase CLI

--- burgerQueen/src/environments/environment.ts
+++ burgerQueen/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCOT3SeWGapE4JLQ0ab7hs7Lbn9VkaQFFw',
+    authDomain: 'burguerqueen-baebc.firebaseapp.com',
+    projectId: 'burguerqueen-baebc',
+    storageBucket: 'burguerqueen-baebc.appspot.com',
+    messagingSenderId: '887882315276',
+    appId: '1:887882315276:web:7cf89ce6132f8782872029',
+    measurementId: 'G-VGGJH7QGR5',
+  }
 };
 
 /*
