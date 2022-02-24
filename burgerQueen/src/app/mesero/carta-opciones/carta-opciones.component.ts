6b80a66a44d191b43b62dce3b223ceed0f338288
andreahenos <henostroza.andrea@gmail.com>
2022-02-24 17:25:16 -0500

    Corrigiendo nombre de variables (sugerencia)

--- burgerQueen/src/app/mesero/carta-opciones/carta-opciones.component.ts
+++ burgerQueen/src/app/mesero/carta-opciones/carta-opciones.component.ts
@@ -8,17 +8,17 @@ import { ProductService } from 'src/app/services/product.service';
 })
 
 export class CartaOpcionesComponent implements OnInit {
-  public title: any;
-  public price: any;
+  public titleHamburger: any;
+  public priceHamburger: any;
   public typeHamburger: any; //el array donde se va a buscar los datos
   public aditionalHamburger: any;
   public imgHamburger: any;
   constructor(public productService: ProductService) { }
 
   ngOnInit(): void {
-    this.title = this.productService.disparador.getValue().title;
-    this.price = this.productService.disparador.getValue().price;
-    this.typeHamburger = this.productService.disparador.getValue().tipe; //para jalar datos del json
+    this.titleHamburger = this.productService.disparador.getValue().title;
+    this.priceHamburger = this.productService.disparador.getValue().price;
+    this.typeHamburger = this.productService.disparador.getValue().type; //para jalar datos del json
     this.aditionalHamburger = this.productService.disparador.getValue().extra;
     this.imgHamburger = this.productService.disparador.getValue().img;
     console.log(this.typeHamburger);
