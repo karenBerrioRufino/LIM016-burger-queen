f36252287dd72c9f4b335a825ea0d54b11a2ea26
andreahenos <henostroza.andrea@gmail.com>
2022-03-04 22:28:32 -0500

    Subiendo archivo .json a Firebase

--- burgerQueen/src/assets/json/carta/upload_carta.js
+++ burgerQueen/src/assets/json/carta/upload_carta.js
@@ -8,12 +8,47 @@ admin.initializeApp({
 const firestore = admin.firestore();
 const settings = { timestampsInSnapshots: true };
 firestore.settings(settings);
+
 if (data && (typeof data === "object")) {
-    Object.keys(data).forEach(docKey => {
-        firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {
+    data.hamburguesas.forEach(docKey => {
+        firestore.collection(collectionKey).doc().set(docKey).then((res) => {
             console.log("Document " + docKey + " successfully written!", res);
         }).catch((error) => {
             console.error("Error writing document: ", error);
         });
-    });
+    })
+
+    data.sandwiches.forEach(docKey => {
+        firestore.collection(collectionKey).doc().set(docKey).then((res) => {
+            console.log("Document " + docKey + " successfully written!", res);
+        }).catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+    })
+
+    data.aperitivos.forEach(docKey => {
+        firestore.collection(collectionKey).doc().set(docKey).then((res) => {
+            console.log("Document " + docKey + " successfully written!", res);
+        }).catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+    })
+
+    data.bebidas.forEach(docKey => {
+        firestore.collection(collectionKey).doc().set(docKey).then((res) => {
+            console.log("Document " + docKey + " successfully written!", res);
+        }).catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+    })
 }
+
+/*     Object.keys(data).forEach(docKey => {
+        data[docKey].map(item => {
+            firestore.collection(collectionKey).doc(docKey).collection(item.title).doc(item.id).set(item).then((res) => {
+                console.log("Document " + item.title + " successfully written!", res);
+            }).catch((error) => {
+                console.error("Error writing document: ", error);
+            });
+        })
+    }); */
