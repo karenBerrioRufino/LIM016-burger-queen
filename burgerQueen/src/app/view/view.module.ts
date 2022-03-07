cbf6cdd4817879adcc7749a13c233c8cf1b765d2
andreahenos <henostroza.andrea@gmail.com>
2022-03-07 18:49:37 -0500

    Comentando View

--- burgerQueen/src/app/view/view.module.ts
+++ burgerQueen/src/app/view/view.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { SharedModule } from '../shared/shared.module';
-import { PrincipalComponent } from './componentes/principal/principal.component';
-import { MeseroModule } from '../mesero/mesero.module';
+// import { SharedModule } from '../shared/shared.module';
+// import { PrincipalComponent } from './componentes/principal/principal.component';
+// import { MeseroModule } from '../mesero/mesero.module';
 
 
 
 @NgModule({
-  declarations: [PrincipalComponent],
+declarations: [/*PrincipalComponent*/],
   imports: [
     RouterModule,
     CommonModule,
-    SharedModule,
-    MeseroModule,
+    // SharedModule,
+    // MeseroModule,
   
   ],
    exports:[
-    PrincipalComponent,
+    // PrincipalComponent,
    
   ]
 })
