5a3d6364c288503eb2fc76ac1bd4d8a18004ea62
kalyzca <alcantarakaly@gmail.com>
2022-03-21 10:12:40 -0500

    validaciones login con uso de toast sweet alert

--- burgerQueen/src/app/ingreso/login/login.component.ts
+++ burgerQueen/src/app/ingreso/login/login.component.ts
@@ -1,10 +1,11 @@
+import { SelectorMatcher } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 // import { MeseroModule } from 'src/app/mesero/mesero.module';
 import { Router } from '@angular/router';
-import { doc } from 'firebase/firestore';
 import { BehaviorSubject } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { createUsersService } from 'src/app/services/create-users.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -14,71 +15,118 @@ import { createUsersService } from 'src/app/services/create-users.service';
 
 export class LoginComponent implements OnInit {
   getRolUser$: BehaviorSubject<string>;
-  title:string="Los angeles de charlie";
+  
   usuario ={
     email:"",
     password:""
   }
 
+  Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3200,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+
   constructor( private authService:AuthService, private router:Router, private createUser : createUsersService) { 
     this.getRolUser$ = this.createUser.getRol();
+    
   }
 
   ngOnInit(): void { 
    
   }
   
-  multiple(uid : any) {
-    this.createUser.getdocUser(uid).subscribe((doc) =>{
-      const rol = doc.payload.data().rol;
-         if (doc.payload.exists) {
-          console.log("Document data:", doc.payload.data());
-          switch(rol){
-            case 'Mesero': this.router.navigateByUrl("/carta")
-            break;
-            case 'Cocinero': this.router.navigateByUrl("/pedidosMesero")
-            break;
-            case 'Administrador': this.router.navigateByUrl("/gestionUsuarios")
-            break;
-            default: this.router.navigateByUrl("/")
-            break;
-          }
-      } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-      }
-      this.getRolUser$.next(rol);
-    return rol;
-    }) 
-  } 
-  
   ingresar(){
     console.log('este es login',this.usuario)
     // desestrucutrar una variable
     const {email, password} = this.usuario;
-
+    this.loginValidator();
+    // this.authService.errorsOcurredLogin(email,password)
     this.authService.login(email, password)
       .then(user => {
         console.log("Bienvenido ", user?.user)
-          if(user && user.user?.emailVerified){
-            console.log(user.user?.emailVerified);
-            console.log(user.user.uid);
-            const idUser = user.user.uid;
-            this.multiple(idUser);
-            return;
-          } 
-          else if(user){
-            console.log('modal para pedir que verifiquesu usuario');
-          } 
+        if(user && user.user?.emailVerified){
+          console.log(user.user?.emailVerified);
+          console.log(user.user.uid);
+          const idUser = user.user.uid;
+          this.multiple(idUser);
+          return;
+        } 
+        else if(user){
+          console.log('modal para pedir que verifiquesu usuario');
+          this.Toast.fire({
+            icon: 'info',
+            title: 'Verifique su correo electrónico y haga click en el link.'
+          })
+        } 
       }).catch(err => {
           console.log(err);
       });
   }
 
+   multiple(uid : any) {
+    this.createUser.getdocUser(uid).subscribe((doc) =>{
+      const rol = doc.payload.data().rol;
+      this.getRolUser$.next(rol);
+      switch(rol){
+        case 'Mesero': this.router.navigateByUrl("/carta")
+        break;
+        case 'Cocinero': this.router.navigateByUrl("/pedidosMesero")
+        break;
+        case 'Administrador': this.router.navigateByUrl("/gestionUsuarios")
+        break;
+        default: this.router.navigateByUrl("/")
+        break;
+          
+      }
+      if (doc.payload.exists) {
+      console.log("Document data:", doc.payload.data());
+      
+      } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+      }
+    return rol;
+    }) 
+  } 
+
   resetPass(){
+  
     console.log("SWEET ALERT PARA PEDIR QUE REVISE EL CORREO Y RESTABLEZCA SU CONTRASEÑA");
     const {email} = this.usuario;
     this.authService.resetPassword(email);
+    this.Toast.fire({
+        icon: 'info',
+        title: 'Revise su correo electrónico y restablezca su contraseña.'
+    })
     console.log('Email de reseteo enviado');
   }
+
+  loginValidator(){
+    
+    if((this.usuario.email === '') || (this.usuario.password === '')){
+      console.log('Debes completar todos los campos');
+      this.Toast.fire({
+        icon: 'error',
+        title: 'Debes completar todos los campos.'
+      })
+    }
+    if (this.usuario.email!=='' && this.usuario.password!=='') {
+      if (this.usuario.password.length<6){
+        this.Toast.fire({
+          icon: 'error',
+          title: 'El password debe contener minimo 6 caracteres.'
+        })
+      }  
+      
+    }
+  
+  }
+  
 }
