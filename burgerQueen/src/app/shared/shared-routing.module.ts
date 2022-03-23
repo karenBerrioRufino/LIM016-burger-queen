9796e371511e3cf8382e74fab8fd6c93411e2e07
Karen <krufino@itech-sac.com>
2022-03-22 23:10:11 -0500

    viewOrder component

---
+++ burgerQueen/src/app/shared/shared-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { TotalPedidosComponent } from './total-pedidos/total-pedidos.component';
+import { ViewOrderComponent } from './view-order/view-order.component';
+
+const routes: Routes = [
+  {
+    path: 'pageNotFound', component: PageNotFoundComponent
+  },
+  {
+    path: 'totalPedidos', component: TotalPedidosComponent
+  },
+  {
+    path: 'viewOrder', component: ViewOrderComponent
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class SharedRoutingModule { }
+
